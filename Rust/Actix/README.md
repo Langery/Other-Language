7a9e5fb2639589ed383a82d6a3f7bf95151ce1ce
Langery <1245066197@qq.com>
2023-03-20 11:59:14 +0800

    add demo

--- Rust/Actix/README.md
+++ Rust/Actix/README.md
@@ -3,3 +3,8 @@
 ### 教程
 
 - [Actix-Web 搭建 Websocket 开发框架](https://zhuanlan.zhihu.com/p/421571942)
+
+### Note
+
+1. 注意加载依赖；(后续总结依赖包)
+2. websocket 待学习...
--- Rust/Actix/actor-ping/src/main.rs
+++ Rust/Actix/actor-ping/src/main.rs
@@ -1,5 +1,46 @@
-use actix_web::{App, HttpResponse, HttpServer, get};
+use actix::Actor;
+use actix_web::{App, HttpResponse, HttpServer, get, HttpRequest, web};//HttpRequest, web
+use actix_web_actors::ws::{WebsocketContext};
 
+// create a object
+pub struct WsConn{
+    pub nick: String
+}
+
+impl Actor for WsConn {
+    type Context = WebsocketContext<Self>;
+
+    // in
+    fn started(&mut self, _: &mut Self::Context){
+        println!("{} join!", self.nick);
+    }
+    // out
+    fn stopped(&mut self, _: &mut Self::Context){
+        println!("{} exit!", self.nick);
+    }
+}
+
+// Tip 2
+#[get("/ws")]
+async fn index(req: HttpRequest, stream: web::Payload,) -> HttpResponse {
+    let conn = WsConn {
+        nick: "Langery".into()
+    };
+    let resp = actix_web_actors::ws::start(conn, &req, stream);
+    match resp {
+        Ok(ret) => ret,
+        Err(e) => e.error_response(),
+    }
+}
+
+// Tip 1
+// #[get("/ws")]
+// async fn index() -> HttpResponse {
+//     HttpResponse::Ok()
+//         .body("Hello World")
+// }
+
+// create app
 pub async fn create_app() {
     let (addr, port) = ("0.0.0.0", "8080");
 
@@ -11,12 +52,6 @@ pub async fn create_app() {
         .expect(&format!("Can't bind to port {}", port))
         .run()
         .await;
-}
-
-#[get("/ws")]
-async fn index() -> HttpResponse {
-    HttpResponse::Ok()
-        .body("Hello World")
 }
 
 #[actix_web::main]
